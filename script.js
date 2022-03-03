d51e312792ba4884b301c2e92927afccbb6cfd3d
ayush singhal <ayushsinghalmains@gmail.com>
2022-03-03 12:15:49 +0530

    time feature added

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const Mins=document.getElementById("Mins");
 const Secs=document.getElementById("Secs");
 const btn=document.getElementById("btn");
-let Minutes=4;
-let Seconds=59;
-let sum=0;
 let Easy0=["each","face","fail","hack","lack","safe","card","frog","math","jump","fish","car","dog","php",
   "box","dear","neck","four","cow","word","list","blog","eye","yes","save","pen"];
 let Easy1=["queen","about","enjoy","allow","alive","great","human","lucky","laugh","spark","touch","whole","water",
@@ -12,10 +9,15 @@ let Easy1=["queen","about","enjoy","allow","alive","great","human","lucky","laug
    let Easy2=["advice","annual","better","border","bright","camera","centre","credit","either","factor","family","female","finger","groud","income","manage",
    "master","minute","second","mirror","palace","player","prison","relate","should","single","street","trying","wholly","certain","college","compact","concept","current","despite","discuss","driving",
    "essence","example","explain","fashion","fifteen","general","history","justify","kitchen","logical","offense","optical","plastic","poverty","profile"];
+   let Minutes=1;
+let Seconds=59;
+let flag=false;
+let sum=0;
 btn.addEventListener("click",()=>{
     var getSelectedValue = document.querySelector(   
         'input[name="label"]:checked');
-        if(getSelectedValue != null) {     
+        if(getSelectedValue != null) { 
+            flag=true;    
             let lavel=getSelectedValue.value;
                GameStart(lavel);
         }
@@ -24,8 +26,8 @@ btn.addEventListener("click",()=>{
              alert("🙄please select a lavel🙄")
          }
 });
-function GameStart(a)
-{
+ function GameStart(a)
+{       
     let b=[Easy0,Easy1,Easy2];
     let selectarray=b[a];
     let i;
@@ -41,16 +43,15 @@ function GameStart(a)
             Newword+=arrayword[index];
             arrayword.splice(index,1);
         }
+    document.getElementById("ans-word").style.display="block";
     document.getElementById("ques-word").innerHTML=Newword;
     document.getElementById("btn").style.display="none";
     document.getElementById("lavel").style.display="none";
     document.getElementById("btn1").style.display="block";
     let input=document.getElementById("ans-word");
     input.addEventListener("keyup",()=>{
-
         let ans=document.getElementById("ans-word").value.trim();
         if(ans==actualword){
-   
               sum++;
               GameStart(a);
              input.value="";
@@ -63,18 +64,29 @@ function GameStart(a)
 btn1.addEventListener("click",()=>{
     location.reload();
 })
- function timefunction(sum)
-{
-    console.log(Minutes+" "+Seconds);
-   Seconds--;
-   if(Seconds<1&& Minutes>0)
-   {
-       Seconds=59;
-       Minutes--
-   }
-   else if(Seconds<1&&Minutes==0&&sum<10)
-         {
-             alert("😔 You lose! let's try again😔")
-             location.reload();
-         }
-}
+const time=setInterval(() => {
+    if(Seconds<=59&&Seconds>0&&Minutes>=0&&sum<10&&flag){
+          Seconds--;
+        document.getElementById("Mins").innerHTML=Minutes+" M";
+        document.getElementById("Secs").innerHTML=Seconds+" S";
+    }
+    else if(Seconds==0&&Minutes>0&&sum<10&&flag)
+      {
+          Minutes--;
+          Seconds=59;
+          document.getElementById("Mins").innerHTML=Minutes+"M";
+          document.getElementById("Secs").innerHTML=Seconds+"S";
+      }
+    else if(Minutes==0&&Seconds==0&&sum<10&&flag)
+    {
+         alert("😔AHH! YOU LOSE,LET'S TRY AGAIN 👉😊");
+         clearInterval(time);
+         location.reload();
+    }
+    else if((Minutes!=0||Seconds!=0||sum==10)&&flag)
+      {
+          alert("🙌HEY CONGRATS! YOU WON, LET'S DO AGAIN👉😊");
+          clearInterval(time);
+          location.reload();
+      }
+}, 1000);
